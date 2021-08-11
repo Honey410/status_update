fc4143bcc5e7cb7754bfc0cda6f8c8c8c72ce81b
Honey410 <82690870+Honey410@users.noreply.github.com>
2021-08-11 17:15:13 +0530

    Revert "Cocoon and Form"

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,12 +7,7 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "bootstrap"
 
-import "cocoon";
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
-require('jquery')
-require('popper.js');
-
