3165128cf38c3bdf1d9899b2d2b837a48bde9949
Honey410 <honayjhamnani410@gmail.com>
2021-08-12 18:01:02 +0530

    Action mailer and front-end

--- app/mailers/status_mailer.rb
+++ app/mailers/status_mailer.rb
@@ -1,8 +1,7 @@
 class StatusMailer < ApplicationMailer
   def new_status_email
     @status = params[:status]
-
-    mail(to: 'bennyjhamnani@gmail.com', subject: "Training Status #{@status.date}")
-    
+    mail(to: '171210107020@aiet.edu.in', subject: "Training-Status-#{@status.date}")
   end
+  
 end
